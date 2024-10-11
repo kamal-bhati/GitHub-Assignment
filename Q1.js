7d63c81c85d52ca34f07d2248c2c59278cebfc81
Kamal Singh <bhatik014@gmail.com>
2024-10-11 10:49:43 +0530

    Q1 solution

--- Q1.js
+++ Q1.js
@@ -7,24 +7,5 @@ console.log(b)
 square(a)
 
 
-let a2 = [1,2,3,4,5];
-function evenNumber(a){
-let b1=a.filter(n=>n%2==0?n:null);
-console.log(b1)
-}
-evenNumber(a2)
 
 
-let a3 = [1,2,3,4,5];
-function sumNumber(a){
-let b1=a.reduce((ac,num)=>ac+num,0);
-console.log("Sum is : ",b1)
-} 
-sumNumber(a3)
-
-let a4 = [1,2,3,4,5];
-function sumOfEvenSquars(a){
-let b1=a.filter(n=>n%2==0?n:null).map(n=>n*n).reduce((ac,num)=>ac+num,0);
-console.log("Sum of even numbers squar is : ",b1)
-}
-sumOfEvenSquars(a4)
